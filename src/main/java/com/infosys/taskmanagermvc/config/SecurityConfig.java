2f610d3c5f432e320170711749e1d3a005903abd
gajiniahmed343 <gajiniahmed343@gmail.com>
2025-04-29 17:46:07 +0530

    Updated code

--- src/main/java/com/infosys/taskmanagermvc/config/SecurityConfig.java
+++ src/main/java/com/infosys/taskmanagermvc/config/SecurityConfig.java
@@ -49,9 +49,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/login", "/register", "/tasks/filter/**")  // Disable CSRF for login and register
-                )
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/", "/login", "/register").permitAll()  // Public routes
